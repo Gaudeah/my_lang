27fad3c41d91456887d98873b74550692e46bb7f
gaudeah <gaudea_h@etna-alternance.net>
2017-03-31 11:04:07 +0200

    fin du lexer, rules revues

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 var fs = require('fs');
 
-var rules = (
-    {IF : "if"},
-    {LEFT_PARENT : "\("},
-    {RIGHT_PARENT : "\)"},
-    {LEFT_BRACE : "\{"},
-    {RIGHT_BRACE : "\}"},
-    {VAR : "var"},
-    {EQUAL : "="},
-    {INTEGER : "\d+"},
-    {TEXT : "[\w\d$]+"}
-);
+var rules = [
+    { name: "IF", pattern: /^if/ },
+    { name: "LEFT_PARENT", pattern: /^\(/ },
+    { name: "RIGHT_PARENT", pattern: /^\)/ },
+    { name: "LEFT_BRACE", pattern: /^\{/ },
+    { name: "RIGHT_BRACE", pattern: /^\}/ },
+    { name: "VAR", pattern: /^var/ },
+    { name: "EQUAL", pattern: /^=/ },
+    { name: "INTEGER", pattern: /^\d+/ },
+    { name: "STRING", pattern: /^[\'\"][\w\d$]+[\'\"]/ },
+    { name: "TEXT", pattern: /^[\w\d$]+/ },
+    {name : "ENDL", pattern: /^;/}
+];
 
+function error(message) {
+    console.log(message);
+    process.exit(1);
+}
+
+function tokenize() {
+    var file;
+    var result = Array();
+
+    if (process.argv[2] == undefined) error("Please enter a file name");
+    if ((file = fs.readFileSync(process.argv[2], 'utf8'))) {
+        file = file.replace('\\n', '\n').replace('\\r', '\r');
+        while (file.length > 0) {
+            var found = false;
+            file = file.trim();
+            if (file.length <= 0) break;
+            for (var rule of rules) {
+                var regex = new RegExp(rule.pattern);
+                var match = file.match(regex);
+                if (match) {
+                    found = true;
+                    match = match[0];
+                    result.push({ name: rule.name, value: match });
+                    file = file.slice(match.length);
+                    break;
+                }
+            }
+            if (!found) error("Token not found : " + file);
+        }
+        console.log(result);
+    }
+}
+
+tokenize();
