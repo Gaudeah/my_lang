ca822f9648f455f89641c0d89de7b0473d207c39
gaudeah <gaudea_h@etna-alternance.net>
2017-03-31 19:37:32 +0200

    add conditions function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
 
+var variables = Array();
+var functions = Array();
+
 var funcs = [
     { IF: if_func },
     { VAR: var_func },
@@ -15,6 +18,8 @@ var rules = [
     { name: "VAR", pattern: /^var\s/ },
     { name: "COMPARE", pattern: /^==/ },
     { name: "EQUAL", pattern: /^=/ },
+    { name: "EQUAL", pattern: /^</ },
+    { name: "EQUAL", pattern: /^>/ },
     { name: "INTEGER", pattern: /^\d+/ },
     { name: "FUNC", pattern: /^function\s/ },
     { name: "STRING", pattern: /^[\'\"][^\'\"]*[\'\"]/ },
@@ -55,8 +60,50 @@ function tokenize() {
     return (results);
 }
 
+function trait_condition(conditions, results) {
+    var verif = false;
+    var count = 0;
+
+    if (!conditions) return false;
+    for (condition of conditions) {
+        if (condition === "==" || condition === "<" || condition === ">") {
+            var operand = condition;
+            verif = true;
+        }
+        count++;
+    }
+    if (verif === false) {
+        for (variable of variables) {
+            if (variable.name === conditions[count - 1]) {
+                console.log("Variable exists");
+                break;
+            }
+        }
+    }
+    else {
+        if (operand === "==") {
+            for (variable of variables) {
+                if (variable.value === conditions[count - 1]) console.log("If ok");
+            }
+        }
+        else if (operand === "<") {
+            for (variable of variables) {
+                if (conditions[count - 1] < variable.value && variable.name == conditions[count + 1]) console.log("If ok");
+            }
+        }
+        else if (operand === ">") {
+            for (variable of variables) {
+                if (conditions[count - 1] > variable.value) console.log("If ok");
+            }
+        }
+        else
+        error("The condition doesn't work");
+    }
+
+}
+
 function if_func(results) {
-    var condition = Array();
+    var conditions = Array();
     var fill = Array();
 
     if (!results[0] || results[0].name !== "LEFT_PARENT")
@@ -64,7 +111,7 @@ function if_func(results) {
     results.splice(0, 1)[0].value;
 
     while (results[0].name != "RIGHT_PARENT") {
-        condition.push(results[0].value);
+        conditions.push(results[0].value);
         results.splice(0, 1)[0].value;
     }
 
@@ -85,7 +132,8 @@ function if_func(results) {
         error("Unexpected identifier : " + results[0].value);
     results.splice(0, 1)[0].value;
 
-    console.log('New if : ' + 'conditions = ' + JSON.stringify(condition) + ' to do = ' + JSON.stringify(fill));
+    trait_condition(conditions, results);
+    // console.log('New if : ' + 'conditions = ' + JSON.stringify(condition) + ' to do = ' + JSON.stringify(fill));
 }
 
 function var_func(results) {
@@ -99,9 +147,10 @@ function var_func(results) {
 
     if (!results[0] || (results[0].name !== "STRING" && results[0].name !== "INTEGER"))
         error("Unexpected identifier : " + results[0].value);
+    variables.push({ name: name, value: results[0].value });
     var value = { type: results[0].name, value: results.splice(0, 1)[0].value };
 
-    console.log('New var : ' + name + " : " + JSON.stringify(value));
+    // console.log('New var : ' + name + " : " + JSON.stringify(value));
 }
 
 function function_func(results) {
@@ -120,7 +169,7 @@ function function_func(results) {
         argv.push(results[0].value);
         results.splice(0, 1)[0].value;
     }
-    
+
     if (!results[0] || results[0].name !== "RIGHT_PARENT")
         error("Unexpected identifier : " + results[0].value);
     results.splice(0, 1)[0].value;
@@ -138,7 +187,8 @@ function function_func(results) {
         error("Unexpected identifier : " + results[0].value);
     results.splice(0, 1)[0].value;
 
-    console.log('New func : ' + 'arguments = ' + JSON.stringify(argv) + ' to do = ' + JSON.stringify(fill));
+    functions.push({ name: name });
+    // console.log('New func : ' + 'arguments = ' + JSON.stringify(argv) + ' to do = ' + JSON.stringify(fill));
 }
 
 function parsing(results) {
@@ -161,7 +211,7 @@ function parsing(results) {
     }
 }
 
-console.log("debut tokenization");
 var tokens = tokenize();
-console.log("debut parsing");
-parsing(tokens);
+console.log("Fin de la tokenization");
+var parsed = parsing(tokens);
+console.log("Fin du parsing");
