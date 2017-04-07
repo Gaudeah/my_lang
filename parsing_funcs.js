1e01281a94adfd9ae08ebb04ecc46eac5abe06cd
gaudeah <gaudea_h@etna-alternance.net>
2017-04-07 09:28:30 +0200

    clean project

--- parsing_funcs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = [
-    { IF: require('./if_func.js') },
-    { VAR: require('./var_func.js') },
-    { FUNC: require('./function_func.js') },
-    { TEXT: require('./text_func.js') }
-];
